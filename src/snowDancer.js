4870b34ca8ec378e4e3de27f9a32adf8fe229c6d
AC Roselee <acroselee@gmail.com>
2019-01-13 18:50:38 -0800

    Safety commit - mousever changes size of dancers

--- src/snowDancer.js
+++ src/snowDancer.js
@@ -1,39 +1,28 @@
- var snowDancer = function(top, left, timeBetweenSteps) {
-   makeDancer.call(this, top, left, timeBetweenSteps);
-   this.$node.addClass('snowflake');
-
- };
-
- snowDancer.prototype = Object.create(makeDancer.prototype);
- snowDancer.prototype.constructor = snowDancer;
-
- snowDancer.prototype.step = function() {
-   makeDancer.prototype.step.call(this);
-   this.$node.fadeToggle();
-
-   $('.dancer.snowflake').on('mouseover', function() {
-     // $(".dancer.snowflake").stop();
-     $(".dancer.snowflake").animate([{
-       transform: 'scale(1)',
-       offeset: 0
-     }, {
-       transform: 'scale(2)',
-       offeset: 0
-     }], {
-       duration: 3000,
-       easing: 'linear',
-       delay: 10,
-       iteration: Infinity,
-       direction: 'alternate',
-       fill: 'forwards'
-     });
+var snowDancer = function(top, left, timeBetweenSteps) {
+  makeDancer.call(this, top, left, timeBetweenSteps);
+   this.$node.addClass("snowflake");
+   var snowyDancer = this;
+   this.$node.on('mouseover', function (event) {
+  //  console.log("mouseover!")
+    var styleSettings = {
+    height: 80,
+    width: 80
+  };
+    snowyDancer.$node.css(styleSettings);
    });
 
- };
+};
 
- makeDancer.prototype.lineUp = function() {
-   this.$node.toggle(true);
-   this.$node.animate({
-     left: '200px'
-   }, 5000);
- };
+snowDancer.prototype = Object.create(makeDancer.prototype);
+snowDancer.prototype.constructor = snowDancer;
+
+snowDancer.prototype.step = function() {
+    makeDancer.prototype.step.call(this);
+    this.$node.fadeToggle();
+    //this.$node.toggle();
+};
+
+makeDancer.prototype.lineUp = function(){
+  this.$node.toggle(true);
+  this.$node.animate({left: "200px"}, 3000);
+};
--- src/starDancer.js
+++ src/starDancer.js
@@ -1,7 +1,18 @@
 var starDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
-  // this.$node = $('<span class="star-five"></span>') || this.$node;
-  this.$node.addClass('star-five');
+ // this.$node = $('<span class="star-five"></span>') || this.$node;
+ this.$node.addClass("star-five");
+   var starDancer = this;
+    this.$node.on('mouseover', function() {
+      // self.$node.stop();
+    //  console.log('josh rulz');
+   //   starDancer.$node.rotate({ count:4, duration:0.2, easing:'ease-out' });
+    var styleSettings = {
+    height: 80,
+    width: 80
+  };
+    starDancer.$node.css(styleSettings);
+    });
 
 };
 
@@ -9,20 +20,15 @@ starDancer.prototype = Object.create(makeDancer.prototype);
 starDancer.prototype.constructor = starDancer;
 
 starDancer.prototype.step = function() {
-  makeDancer.prototype.step.call(this);
-  this.$node.toggle('bounce');
+    makeDancer.prototype.step.call(this);
+   this.$node.toggle('bounce');
 
-  this.$node.on('mouseover', function() {
-    $('.dancer.star-five').animate({
-      width: '70%',
-      opacity: 0.4
-    }, 1500);
-  });
-};
+ };
 
-makeDancer.prototype.lineUp = function() {
+ makeDancer.prototype.lineUp = function(){
   this.$node.toggle(true);
-  this.$node.animate({
-    left: '200px'
-  }, 5000);
+  this.$node.animate({left: "200px"}, 3000);
 };
+
+
+
